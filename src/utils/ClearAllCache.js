3894859812a7060f0661713d37382c53d3fcbc61
baiyun <baiyun186@gmail.com>
2024-02-23 22:58:06 +0800

    chore: cleanup

--- src/utils/ClearAllCache.js
+++
@@ -1,9 +0,0 @@
-const ClearAllCache = () => {
-    // 将localStorage中的数据全部清除
-    window.localStorage.clear();
-    // 将sessionStorage中的数据全部清除
-    window.sessionStorage.clear();
-    // 将indexedDB中的数据全部清除
-    window.indexedDB.deleteDatabase("BitBoxTools");
-}
-export default ClearAllCache;
--- src/utils/getZksyncData/getFee.js
+++
@@ -1,9 +0,0 @@
-export const getFee = async (transactions) => {
-    let fee = 0;
-    transactions.forEach((transaction) => {
-        const tmpFees = parseInt(transaction.fee, 16) * 10 ** -18 * transaction.ethValue;
-        fee += tmpFees;
-    });
-    return fee === 0 ? 0 : fee.toFixed(2);
-}
-
